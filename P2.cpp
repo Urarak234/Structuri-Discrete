c8e57c5fee9361320ecfc2f2ed24dfb6bc8c5f51
Porcescu Dmitri <71259338+Urarak234@users.noreply.github.com>
2020-11-04 10:08:29 +0300

    Create P2.cpp

---
+++ P2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+long unsigned Factorial (int n){
+
+}
+
+int main()
+{
+
+    int n;
+    cout << "Proect 0_2, elaborat de Porcescu Dmitri." << endl;
+    cout << "Tema: Elemente din combinatorica.\n";
+    cout << "Aranjamente, permutari, combnari.\n";
+
+    cout << "Vvedite cislo: ";
+    cin >> n;
+    cout << n <<"! = "<< Factorial(n) << endl;
+
+
+    return 0;
+}
